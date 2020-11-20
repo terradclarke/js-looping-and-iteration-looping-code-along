6dc0b38a34891705562c718525b215cf700c2ebe
Terradclarke <terranie67@gmail.com>
2020-11-19 21:10:24 -0600

    commit tree 701a8314f123154122cb230062e675e4dce28010

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+
+names = [ 'Lisa', 'Kaitlin', 'Jan' ]
+event = "surprise"
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        console.log('Thank you $(name) for the wonderful $(event) gift!')
+    }
+}
+
+function printBadges(names) {
+    for(let i = 0; i < names.length; i++) {
+        console.log("Welcome " + names[i] + `! You are employee #${i+1}.`)
+    }
+    return names;
+}
+
+function tailsNeverFails() {
+    let i = 0;
+    while (Math.random() >= 0.5) {
+        i ++;
+    }
+    return `You got ${i} tails in a row!`;
+}
